d95f46551dfc6faabe00584960ff88a73772361d
basting <bastin.gomez@gmail.com>
2015-09-13 20:45:32 -0400

    Changes

--- app/src/main/java/com/bgh/calltimer/service/CallMonitoringService.java
+++ app/src/main/java/com/bgh/calltimer/service/CallMonitoringService.java
@@ -1,7 +1,21 @@
 package com.bgh.calltimer.service;
 
+import android.app.IntentService;
+import android.content.Intent;
+
 /**
  * Created by BastinGomez on 2015-06-21.
+ *
+ * Monitoring service for minutes usage
  */
-public class CallMonitoringService {
+public class CallMonitoringService extends IntentService{
+
+    public CallMonitoringService(String name){
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
 }
